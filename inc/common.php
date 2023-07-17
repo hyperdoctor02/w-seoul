f07ca93d53b58a02b4973ec26b99fe364b069525
hyperdoctor <hyperdoctor@gamil.com>
2023-07-17 15:17:46 +0900

    first commit

---
+++ inc/common.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+<title>서울W내과</title>
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="viewport" content="width=1320px,user-scalable=yes"/>
+<link rel="stylesheet" type="text/css" href="/css/common.css"/>
+<link rel="stylesheet" type="text/css" href="/css/global.css"/>
+<link rel="stylesheet" type="text/css" href="/css/board.css"/>
+<script src="/js/jquery.min.js"></script>
+<script src="/js/java.js"></script>
+<script src="/js/global.js"></script>
+<!--[if lt IE 9]>
+<script src="/js/html5.js"></script>
+<![endif]-->
+
+<!-- s:Naver site verification -->
+<meta name="naver-site-verification" content="a10cfc28fba9f9f10a56278032b8f58462eb25e3"/>
+<meta name="description" content="서울대입구역 위치 내과, 장정결제 없이 대장내시경, 당일 내시경, 건강검진, 공단검진">
+<!-- e:Naver site verification -->
+<!-- s:Open graph -->
+<meta property="og:type" content="website">
+<meta property="og:title" content="서울W내과 (서울더블유내과)">
+<meta property="og:description" content="서울대입구역 위치 내과, 장정결제 없이 대장내시경, 당일 내시경, 건강검진, 공단검진">
+<meta property="og:image" content="http://www.w-medicalcenter.com/images/logo.gif">
+<meta property="og:url" content="http://www.w-medicalcenter.com/">
+<!-- e:Open graph -->
---
+++ js/global.js
@@ -0,0 +1,319 @@
+function MM_swapImgRestore() { //v3.0
+var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+function MM_preloadImages() { //v3.0
+var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) { //v4.01
+var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() { //v3.0
+var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+
+
+//1.스크립트 위로 튕기는것
+$(document).on('click', 'a[href="#"]', function(e){
+    e.preventDefault();
+});
+
+
+
+//VISUAL
+var vis = {
+	init : function(){
+		this.action();
+	},
+	action : function(){
+		var set = {
+			roll : $('.section1 .roll')
+		};
+		set.roll.bxSlider({
+			auto: true,
+			useCSS: false,
+			controls: false,
+			pager: false,
+			pause: 6000,
+			speed: 1200,
+			touchEnabled: true,
+			stopAutoOnClick: true,
+			easing: 'easeInOutExpo'
+		});
+	}
+}
+
+
+//VISUAL
+var sec04 = {
+	init : function(){
+		this.action();
+	},
+	action : function(){
+		var set = {
+			roll : $('.sec04 .roll')
+		};
+		set.roll.bxSlider({
+			auto: true,
+			useCSS: false,
+			controls: true,
+			pager: false,
+			pause: 4000,
+			speed: 500,
+			touchEnabled: true,
+			stopAutoOnClick: true,
+			easing: 'easeInOutExpo'
+		});
+	}
+}
+
+
+
+//Popup Images
+$(function(){
+	
+	$('.sec02img').click(function(e){
+			e.preventDefault();
+
+			$('.popimg_bg').stop().fadeIn();
+			$(this).next('.popimg').stop().fadeIn();
+
+			if($(this).index('.sec02img') > 4){
+				$('.popimg').css({'margin-left':'-700px'});
+			}else{
+				$('.popimg').css({'margin-left':'0'});
+			}
+				
+	});
+
+//Popup Background
+	$('.popClose, .popimg_bg').click(function(e){
+		e.preventDefault();
+		$('.popimg').fadeOut();
+		$('.popimg_bg').fadeOut();
+	});
+
+});
+
+
+//버튼 위치 이동 스크롤
+$(window).load(function(){
+
+	var win = $(window);
+	var sec = $('.section');
+	var btnActive = $('.gnb li');
+	var btnSlide = $('.gnb li');
+	var wh = []; //높이 배열
+	var secNum = 0;
+	var spd = 600;
+
+	sec.each(function(i){
+		wh[i] = sec.eq(i).offset().top;
+	});
+
+
+	btnSlide.on({
+		'click' : function(){
+			var i = $(this).index();
+			var pos = $('.section').eq(i).offset().top-55;
+			
+			$('html,body').animate({
+				'scrollTop' : pos
+			},600,'easeInOutQuart');
+
+			return false;
+		}
+
+	});
+
+
+
+
+
+	
+	function acc(){
+		btnActive.eq(secNum).addClass('active').siblings().removeClass('active');
+	}
+	acc();
+
+	win.on({
+		'scroll' : function(){
+			var sh = $(this).scrollTop();
+			
+			for (i=0;i<sec.length;i++){ // 위치값 확인
+				if(sh >= parseInt(wh[i]) - (win.height()/2)){
+					secNum = i;
+					acc();
+				}
+			}
+
+		}
+	});
+});
+
+
+
+var main = {
+	init : function(){
+		this.action();
+	},
+	action : function(){
+		var set = {
+			win : $(window),
+			app : $('.appear'),
+			motion : $('.motion')
+		};
+
+		var spd = 900;
+		set.motion.css('opacity',0);
+
+		set.app.appear();
+
+		set.app.each(function(i){
+			$(this).on({
+				'appear' : function(){
+					$(this).find('.motion').each(function(i){
+						$(this).delay(800).delay(i*500).animate({'top':0,'opacity':1},spd);
+					});
+				}
+			});
+		});
+
+	}
+}
+
+
+var bg = {
+	init : function(){
+		if(!/iPhone|iPad|iPod|BlackBerry|Android|Windows CE|LG|MOT|SAMSUNG|SonyEricsson|webOS|IEMobile|Opera Mini/i.test(navigator.userAgent) ) {
+			this.action();
+		}
+	},
+	action : function(){
+		var set = {
+			hb : $('html,body'),
+			win : $(window),
+			bg : $('.bg')
+		};
+
+		var bgNum = 0;
+		var bh = [];
+		var wh = set.win.height();
+
+		set.bg.each(function(i){
+			bh[i] = $(this).offset().top;
+		});
+
+		set.bg.appear();
+
+		var sh = 0;
+
+		set.win.on({
+			'scroll' : function(){
+				sh = $(this).scrollTop();
+			}
+		});
+
+		set.bg.eq(0).css({"backgroundPosition": "50% "+ ((bh[0] - sh) * 0.03) +"px"});
+
+		set.bg.each(function(i){
+			$(this).on('appear',function(){
+				$(this).css({"backgroundPosition": "50% "+ ((bh[i] - sh) * 0.03) +"px"});
+			});
+		});
+
+	}
+}
+
+function Aniscroll(){
+
+	var ua = window.navigator.userAgent;
+	var msie = ua.indexOf("MSIE ");
+
+	if (msie > 0 || !!navigator.userAgent.match(/Trident.*rv\:11\./)) {
+			jQuery.extend(jQuery.easing, {
+					easeOutQuint: function(x, t, b, c, d) {
+							return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+					}
+			});
+
+			if($('html.nshtml' == false)){
+
+			setTimeout(function(){
+					var wheel = false,
+									$docH = $(document).height() - $(window).height(),
+									$scrollTop = $(window).scrollTop();
+
+					$(window).bind('scroll', function() {
+							if (wheel === false) {
+									$scrollTop = $(this).scrollTop();
+							}
+					});
+					$(document).bind('DOMMouseScroll mousewheel', function(e, delta) {
+							delta = delta || -e.originalEvent.detail / 3 || e.originalEvent.wheelDelta / 120;
+							wheel = true;
+
+							$scrollTop = Math.min($docH, Math.max(0, parseInt($scrollTop - delta * 50)));
+
+							$($.browser.webkit ? 'body' : 'html').stop().animate({
+									scrollTop: $scrollTop + 'px'
+							}, 1000, 'easeOutQuint', function() {
+									wheel = false;
+							});
+							return false;
+					});
+			}, 1000);
+
+			}
+	}
+}
+
+
+
+
+$(function(){
+	//vis.init();//VIS
+	sec04.init();//SEC04
+	main.init();//MAIN
+	bg.init();//BG
+});
+
+// 상단이동 부드럽게
+$(function () {
+    $(".goTop").on("click", function () {
+        let top = $("body").offset().top;
+        $("html, body").animate({ scrollTop: top }, 800);
+    });
+  });
+
+
+  /*menuOpen*/
+  $(function(){
+    $('.menuOpen button.open').on('click', function(){
+        $('.menuOpen .menuWrap').addClass('on');
+    });
+        $('.menuOpen .menuWrap .close').on('click',function(){
+            $('.menuOpen .menuWrap').removeClass('on');
+        });
+});
+
+
+
+//mo-section스크롤이동
+$(document).ready(function() {
+    $('.menuOpen #m-gnb li a').on('click', function(e) {
+        e.preventDefault();
+        var targetId = $(this).attr('href');
+        var top = $(targetId).offset().top;
+        $('html, body').animate({ scrollTop: top }, 400);
+    });
+});
